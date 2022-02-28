d3bfbb67a1f0520398b2be866b244f43ce7bcf48
kainpets <pawelstepniak22@gmail.com>
2022-02-28 20:44:31 +0100

    finish the game

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function game() {
-  let score = 0;
+  let playerScore = 0;
+  let computerScore = 0;
   for (let i = 0; i < 5; i++) {
-    playRound(playerPlay(), computerPlay())
+    let resultOfTheGame = playRound(playerPlay(), computerPlay());
+    if (resultOfTheGame === "You win! Rock beats scissors") {
+      playerScore += 1;
+    } else if (resultOfTheGame === "You lose! Paper beats Rock") {
+      computerScore += 1;
+    } else if (resultOfTheGame === "Draw! You both chose Rock") {
+      computerScore += 1;
+      playerScore += 1;
+    }
+  }
+  announceWinner(playerScore, computerScore)
+}
+
+function announceWinner(playerScore, computerScore) {
+  if (playerScore > computerScore) {
+    console.log(`You win the game! Your score was ${playerScore} and the computer's ${computerScore}`);
+  } else if (playerScore === computerScore) {
+    console.log(`You draw the game! Both your scores were: ${playerScore}`);
+  } else {
+    console.log(`You lose the game! Your score was ${playerScore} and the computer's ${computerScore}`)
   }
 }
 
@@ -12,13 +32,13 @@ function playRound(playerSelection, computerSelection) {
   let lose = "You lose! Paper beats Rock";
   let draw = "Draw! You both chose Rock";
 
-  if (player === "rock" && computer === "paper") {
+  if (player === "rock" && computer === "paper" || player === "paper" && computer === "scissors" || player === "scissors" && computer === "rock") {
     console.log(lose)
     return lose;
-  } else if (player === "rock" && computer === "scissors") {
+  } else if (player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper") {
     console.log(win)
     return win;
-  } else if (player === "rock" && computer === "rock") {
+  } else if (player === "rock" && computer === "rock" || player === "paper" && computer === "paper" || player === "scissors" && computer === "scissors") {
     console.log(draw);
     return draw;
   } else {
@@ -27,7 +47,7 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function playerPlay() {
-  let playerChoice = window.prompt("Enter your choice: (rock, paper, scrissors")
+  let playerChoice = window.prompt("Enter your choice: (rock, paper, scrissors)")
   return playerChoice
 }
 
@@ -46,3 +66,4 @@ function getRandomInt() {
   return Math.floor(Math.random() * 3);
 }
 
+game()
