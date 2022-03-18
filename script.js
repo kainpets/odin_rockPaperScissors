0f9dbbc28fffe25b6cd4911082716ce2c42b8f93
kainpets <pawelstepniak22@gmail.com>
2022-03-18 18:48:30 +0100

    Add UI to the game

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+  let playerChoice = "rock";
+  console.log(playerChoice);
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+  let playerChoice = "paper";
+  console.log(playerChoice);
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+  let playerChoice = "scissors";
+  console.log(playerChoice);
+});
+
+
+const resultAnnouncment = document.querySelector("#winner");
+// resultAnnouncment.addEventListener()
+
 function game() {
   let playerScore = 0;
   let computerScore = 0;
+  
+  let resultOfTheGame = playRound(playerChoice, computerPlay());
+  
   for (let i = 0; i < 5; i++) {
-    let resultOfTheGame = playRound(playerPlay(), computerPlay());
-    if (resultOfTheGame === "You win! Rock beats scissors") {
+    if (resultOfTheGame === "You win the round! Rock beats scissors") {
       playerScore += 1;
-    } else if (resultOfTheGame === "You lose! Paper beats Rock") {
+    } else if (resultOfTheGame === "You lose the round! Paper beats Rock") {
       computerScore += 1;
-    } else if (resultOfTheGame === "Draw! You both chose Rock") {
+    } else if (resultOfTheGame === "This round's a draw! You both chose Rock") {
       computerScore += 0;
       playerScore += 0;
     }
@@ -25,13 +49,13 @@ function announceWinner(playerScore, computerScore) {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
-  let player = playerSelection.toString().toLowerCase();
-  let computer = computerSelection.toString().toLowerCase();
+function playRound() {
+  let player = playerChoice;
+  let computer = computerPlay().toString().toLowerCase();
   let win = "You win! Rock beats scissors";
   let lose = "You lose! Paper beats Rock";
   let draw = "Draw! You both chose Rock";
-
+  
   if (player === "rock" && computer === "paper" || player === "paper" && computer === "scissors" || player === "scissors" && computer === "rock") {
     console.log(lose)
     return lose;
@@ -46,10 +70,10 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function playerPlay() {
-  let playerChoice = window.prompt("Enter your choice: (rock, paper, scrissors)")
-  return playerChoice
-}
+// function playerPlay() {
+//   //let playerChoice = window.prompt("Enter your choice: (rock, paper, scrissors)")
+//   //return playerChoice;
+// }
 
 function computerPlay() {
   let randomInt = getRandomInt();
@@ -67,3 +91,4 @@ function getRandomInt() {
 }
 
 game()
+
